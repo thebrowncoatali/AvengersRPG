7d9035905033476ccd2d65dde0fdd4d99c092533
Ali Hamza <thebrowncoatali@gmail.com>
2020-12-20 21:44:11 -0400

    Milestone 2. Game Intro.

--- game.py
+++ game.py
@@ -1,4 +1,19 @@
 class Game():
 
-	def __init__(self):
-		print("Test")
+	def _overview(self):
+		'''
+			Welcome and Overview about game
+			Define a quest 
+		'''
+		print(
+			'The Avengers RPG',
+			'Welcome to the Avengers RPG game',
+			'Hi. I\'m Nick Fury, Our mission is to protect the universe from',
+			'the evil intentions of the titan Thanos.',
+			'This mission will not be easy, you will undoubtedly face many challenges while on this mission',
+			sep='\n'
+		)
+
+	def start(self):
+		self._overview()
+
